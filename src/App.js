08149126f757c0aeabb3bdfba771accd0c6fe8a0
michaelmiller2116 <michael.miller2116@gmail.com>
2018-06-19 00:17:44 -0600

    Added hero image and h1, h3. Styled navbar with expanding bottom border

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import Header from './Components/Header.js'
 import Navbar from './Components/Navbar.js'
-import './App.css';
+
+import './App.css'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
+    return <div className="App">
         <Header />
         <Navbar />
-      </div>
-    );
+        <img className="hero-img" src="assets/store-front.jpg" alt="store front" />
+        <h1>TheM-Poncho</h1>
+        <h3>
+          TheM-Poncho is a project born from two European creative designers
+          who
+        </h3>
+      </div>;
   }
 }
 
-export default App;
+export default App
